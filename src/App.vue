5888e75b24f5fa91300d25bc62ebc1c3853ff768
Feihang027 <3151876512@qq.com>
2026-08-10 16:08:43 +0800

    update docs

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <h1>YFHAPP</h1>
+    <p>个人效率管理系统</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.app {
+  padding: 40px;
+}
+</style>
